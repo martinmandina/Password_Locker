4cc14d86bb8498125ea2ff4ed3c9a2b5d627736a
martinsmandina <martinsmandina@gmail.com>
2020-10-10 17:00:37 +0300

    add class variables

--- passwords.py
+++ passwords.py
@@ -3,4 +3,13 @@ class Passwords:
     Class that generates new instances of passwords
     """
 
-    
+    def __init__(self,login_name,password,website):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            login_name: New contact first name.
+            password: New contact phone number.
+            website : Where the password is used eg instagram.
+        '''
