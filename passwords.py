1d37c3b78df73230b81cf0c5a23bf9711f1c3382
martinsmandina <martinsmandina@gmail.com>
2020-10-11 12:31:45 +0300

    create setup method first to run password tests

--- passwords.py
+++ passwords.py
@@ -1,11 +1,11 @@
 import pyperclip
 
-class passwords:
+class Passwords:
     """
     Class that generates new instances of passwordss.
     """
 
-    passwords_list = [] # Empty passwords list
+    Passwords_list = [] # Empty passwords list
 
     def __init__(self,login_name,password):
 
--- passwords_tests.py
+++ passwords_tests.py
@@ -1,6 +1,12 @@
 import unittest # Importing the unittest module
 import pyperclip
-from passwords import passwords
+from passwords import Passwords
 
+class TestPasswords(unittest.TestCase):
 
+    def setUp(self):
+        '''
+        The Set up method is to run before each test case.
+        '''
+        self.new_Password = Passwords("JohnDoe","johndoe2020")
     
