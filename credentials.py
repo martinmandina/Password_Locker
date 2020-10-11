904e13e2c2ee5572af364343e630353fa03b3fb5
martinsmandina <martinsmandina@gmail.com>
2020-10-11 12:11:00 +0300

    create instances for class credentials

--- credentials.py
+++ credentials.py
@@ -1,2 +1,16 @@
 import pyperclip
 
+class credentials:
+    """
+    Class that generates new instances of credentialss.
+    """
+
+    credentials_list = [] # Empty credentials list
+
+    def __init__(self,account,login_name,password):
+
+      
+
+        self.account = account
+        self.login_name = login_name
+        self.password = password
