44527226dade6c84b852fa461d69a7018f1611df
martinsmandina <martinsmandina@gmail.com>
2020-10-11 12:37:12 +0300

    set up tearDown method in password_test.py

--- passwords_tests.py
+++ passwords_tests.py
@@ -9,4 +9,10 @@ class TestPasswords(unittest.TestCase):
         The Set up method is to run before each test case.
         '''
         self.new_Password = Passwords("JohnDoe","johndoe2020")
+
+    def tearDown(self):
+        '''
+        Using tearDown method ensures after every testrun there is a clean up.
+        tearDown method ensures accurate results.
+        '''
     
