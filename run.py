a8236f8ba107ce18aa0cefc8e7f42016633d6067
martinsmandina <martinsmandina@gmail.com>
2020-10-12 17:35:09 +0300

    add while loop @run.py

--- run.py
+++ run.py
@@ -3,47 +3,46 @@
 from passwords import Passwords
 from credentials import Credentials
 
-    def create_passwords(login_name,password):
-            '''
-            This is a function that creates a new password
-            '''
+def create_passwords(login_name,password):
+        '''
+        This is a function that creates a new password
+        '''
         new_passwords = Passwords(login_name,password)
         return new_passwords
 
-
-        def save_passwords(passwords):
+def save_passwords(passwords):
         '''
         A save_passwords function that takes in a password object.
         '''
-        password.save_passwords()
+        passwords.save_passwords()
 
 
-        def del_passwords(passwords):
+def del_passwords(passwords):
         '''
         This function del_passwords deletes a contact
         '''
         passwords.delete_passwords()
 
 
-    def find_passwords(login_name):
+def find_passwords(login_name):
         '''
         This function matches a password by login_name and returns the passsword.
         '''
-        return passwords.find_by_login_name(login_name)
+        return Passwords.find_by_login_name(login_name)
 
 
-    def check_existing_passwords(login_name):
-        '''
-        This function checks if a password exists with the above login_name and returns a boolean.
-        '''
-        return passwords.passwords_exist(login_name)
+# def check_existing_passwords(login_name):
+#         '''
+#         This function checks if a password exists with the above login_name and returns a boolean.
+#         '''
+#         return Passwords.passwords_exist(login_name)
 
 
-    def display_passwords():
-        '''
-        This function checks and returns all the saved passwords
-        '''
-        return passwords.display_passwords()
+# def display_passwords():
+#         '''
+#         This function checks and returns all the saved passwords
+#         '''
+#         return Passwords.display_passwords()
 
 
     # def copy_login_name(passwords)
@@ -51,12 +50,28 @@ from credentials import Credentials
     #     This Function copies a login_name
     #     '''
     
-    def main():
+def main():
         print("Hello user Welcome to your passwords list. What is your name?")
         user_name = input()
 
         print("Hello {user_name}. what would you like to do?")
         print('\n')
+
+while True:
+                    print("Use these short codes : cc - create a new passwords, dc - display passwords, fc -find a passwords, ex -exit the passwords list ")
+
+                    short_code = input().lower()
+
+                    if short_code == 'cp':
+                            print("New passwords")
+                            print("-"*10)
+
+                            print ("login name ....")
+                            login_name = input()
+
+                        
+                            print("credential ...")
+                            credential = input()
 
 if __name__== '__main__':
         main()         
